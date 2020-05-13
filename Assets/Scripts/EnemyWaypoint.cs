2f8c3de8c28b94a0e30a1acd28e3ed72a7852878
Bryan Carl <thewetcouch@Bryans-MacBook-Pro.local>
2020-05-12 17:16:38 -0700

    began work on enemies

---
+++ Assets/Scripts/EnemyWaypoint.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyWaypoint : MonoBehaviour
+{
+    public NavMeshAgent navMeshAgent;
+    public Transform[] destinations;
+
+    int currentWaypoint;
+
+    void Start()
+    {
+        navMeshAgent.SetDestination(destinations[0].position);
+    }
+
+    void Update()
+    {
+        if (navMeshAgent.remainingDistance < navMeshAgent.stoppingDistance)
+        {
+            currentWaypoint = (currentWaypoint + 1) % destinations.Length;
+            navMeshAgent.SetDestination(destinations[currentWaypoint].position);
+        }
+    }
+}
