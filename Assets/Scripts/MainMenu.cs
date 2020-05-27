23e68c568bb4ef1208c4e91d19a8d6a4c4f69523
Bryan Carl <thewetcouch@Bryans-MacBook-Pro.local>
2020-05-26 20:17:27 -0700

    added main menu functionality

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void selectScene()
+    {
+        switch (this.gameObject.name)
+        {
+            case "Tutorial":
+                SceneManager.LoadScene("TutorialLevel");
+                break;
+            case "Beginner":
+                SceneManager.LoadScene("CubeLevel1");
+                break;
+            case "Difficult":
+                SceneManager.LoadScene("MediumLevel");
+                break;
+        }
+    }
+
+    public void quit()
+    {
+        Application.Quit();
+    }
+}
